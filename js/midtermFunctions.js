f0496dcd0910e881334ff41dd9f58915c14e460f
Xinyi Qiu <66391148+Xy16-5@users.noreply.github.com>
2021-04-04 15:42:53 -0400

    style

--- js/midtermFunctions.js
+++ js/midtermFunctions.js
@@ -24,15 +24,18 @@ Slides Setup
 var slide1 = {
     slideNumber: 1,
     title: "Overview of Crashes in Philadelphia (2017) ",
-    content:"The project is a gateway to the Philadelphia crash statistics in 2017. Here you could have an overall understanding of when, why and how severe the crashes are. On the last page, you could find the specific crashes based on the conditions you put in. In 2017, there are totally 9043 crashes in Philadelphia, which led to ",
+    content:"The project is a gateway to the Philadelphia crash statistics in 2017. Here you could have an overall understanding of when, why and how severe the crashes are. On the last page, you could find the specific crashes based on the conditions you put in. In 2017, there are totally 9043 crashes in Philadelphia, which led to 78 people death and 9661 injury. ",
     bbox: [[39.874438536988166, -75.26596069335938],[40.10486150812275, -74.88418579101562]],
-    style: myStyle1
-
+    style: function(feature) {
+      return {
+        color: "green"
+      }
+    }
   };
 
   var slide2 = {
     slideNumber: 2,
-    title: "title 2 ",
+    title: "Temporal Characteristics of Crashes ",
     content:"content2",
     bbox: [[ 39.88682114233502,  -75.25772094726562],[40.02603705467397, -75.02151489257812]]
   };
@@ -69,10 +72,12 @@ var prevPage = function() {
 var buildPage = function(pageDefinition) {
 
   featureGroup = L.geoJson(parsedData,{
+    style: pageDefinition.style,
     pointToLayer: function(feature,latlng){
-      return L.circleMarker(latlng,pageDefinition.style);
+      return new L.CircleMarker(latlng, {radius: 2,fillOpacity: 0.85 })
     }
   });
+
   if(pageDefinition.slideNumber === 1){
     clusters = L.markerClusterGroup();
     clusters.addLayer(featureGroup);
@@ -106,11 +111,27 @@ var tearDown = function() {
 Data Processing
 ==================*/
 var dataset = "https://raw.githubusercontent.com/Xy16-5/CPLN692-Midterm/main/crashes.geojson";
-var parsedData;
+var parsedData ;
 var featureGroup;
 var clusters;
 var markerlist;
+var fatal = 0;
+var injury = 0;
 
+var fatalsum = function(obj){
+
+  for(i=0;i<obj.length;i=i+1){
+    fatal= fatal+obj[i].properties.fatal
+  }
+  return fatal
+}
+
+var injurysum = function(obj){
+  for(i=0;i<obj.length;i=i+1){
+    injury= injury+obj[i].properties.injury
+  }
+  return injury
+}
 
 
 
@@ -118,14 +139,7 @@ $(".nextbutton").click(nextPage)
 $(".previousbutton").click(prevPage)
 
 
-var myStyle1 = {
-  radius: 4,
-  fillColor: "#ff7800",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8
-};
+
 
 
 $(document).ready(function() {
@@ -134,6 +148,13 @@ $(document).ready(function() {
     buildPage(slides[currentPage]); 
     var crashnumber = parsedData.features.length;
     console.log("Total Crashes in 2017: ", crashnumber);
+    fatalsum(parsedData.features);
+    console.log("Total Fatal in 2017: ", fatal);
+    injurysum(parsedData.features)
+    console.log("Total Injury in 2017: ", injury)
+    
+    
+   
   });
 
   });
